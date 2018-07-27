567f40b8be1371e1cb2d77309126990d91911603
Adriano Lima <adrianolslima@gmail.com>
2018-07-27 00:07:43 -0300

    Ajustes gerais

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -20,19 +20,34 @@ create table tarefa (
   constraint pk_tarefa primary key (id)
 );
 
+create table token_cadastro (
+  id                            bigint auto_increment not null,
+  usuario_id                    bigint,
+  codigo                        varchar(255),
+  constraint uq_token_cadastro_usuario_id unique (usuario_id),
+  constraint pk_token_cadastro primary key (id)
+);
+
 create table usuario (
   id                            bigint auto_increment not null,
   email                         varchar(255),
   senha                         varchar(255),
+  verificado                    tinyint(1) default 0 not null,
   constraint pk_usuario primary key (id)
 );
+
+alter table token_cadastro add constraint fk_token_cadastro_usuario_id foreign key (usuario_id) references usuario (id) on delete restrict on update restrict;
 
 
 # --- !Downs
 
+alter table token_cadastro drop foreign key fk_token_cadastro_usuario_id;
+
 drop table if exists estudo;
 
 drop table if exists tarefa;
+
+drop table if exists token_cadastro;
 
 drop table if exists usuario;
 
