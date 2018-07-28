a803a86a87783fa332b598b53b10870f6001b473
Adriano Lima <adrianolslima@gmail.com>
2018-07-28 13:40:30 -0300

    Redirecionamento em caso de não autorizado

--- app/autenticadores/UsuarioAutenticado.java
+++ app/autenticadores/UsuarioAutenticado.java
@@ -2,12 +2,22 @@ package autenticadores;
 
 import controllers.*;
 import play.mvc.Http.Context;
+import play.mvc.*;
 import play.mvc.Security.Authenticator;
 
 public class UsuarioAutenticado extends Authenticator {
 
 	@Override
 	public String getUsername(Context context) {
+		
 		return context.session().get(ControladorUsuario.AUTH);
+	}
+	
+	@Override
+	public Result onUnauthorized(Context context) {
+		
+		context.flash().put("danger", "Não autorizado!");
+		
+		return redirect(routes.HomeController.index());
 	}
 }
