9f783aac7185138a5cc19b49ca8047122363c72e
Adriano Lima <adrianolslima@gmail.com>
2019-05-01 22:14:34 -0300

    Implementando o acompanhamento por migalhas

---
+++ public/js/migalha.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+	var estudo = $('#estudo').text();
+	var tarefa = $('#tarefa').text();
+
+	if (estudo) {
+
+		$("#migalha").find(".ec0").addClass("btn-success");
+	}
+})
